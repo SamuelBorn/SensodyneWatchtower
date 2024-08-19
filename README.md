2146e51ae8352882c108a1bcb61862e4cd5ea4c5
Samuel Born <58636569+SamuelBorn@users.noreply.github.com>
2024-08-19 20:04:26 +0200

    Initial commit
    inital commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# SensodyneWatchtower
+
+A Python script that regularly monitors the Sensodyne Money Back Guarantee website to detect changes in the offer.
+
+Ideal for users who have difficulty securing weekly redemptions due to limited availability.
+
+The script will check every 3 seconds for changes on the Sensodyne website and send a Pushover notification to your Phone if a change is detected.
+
+## Usage
+
+1. Create [Pushover](https://pushover.net/) account, note down _User Key_ and _API Token_
+1. Install Pushover app on your phone
+1. `git clone https://github.com/SamuelBorn/SensodyneWatchtower.git`
+1. `cd SensodyneWatchtower`
+1. Edit `.env` file with your Pushover User Key and API Token
+1. `pip install -r requirements.txt`
+1. `python3 watchtower.py`
---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import time
+from datetime import datetime
+
+# install requirements with `pip install python-dotenv requests bs4`
+import dotenv
+import requests
+from bs4 import BeautifulSoup
+
+# Download the Pushover app from the App Store or Google Play Store
+# Pushover API keys
+# Load environment variables from .env file
+dotenv.load_dotenv()
+PUSHOVER_TOKEN = os.getenv("PUSHOVER_TOKEN")
+PUSHOVER_USER_KEY = os.getenv("PUSHOVER_USER_KEY")
+
+# Content to monitor
+url = "https://www.erlebe-haleon.de/deals/sensodyne-clinical-repair"
+div_id = "restZahlAnzeige"
+
+
+def get_content(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup.find("div", id=div_id)
+
+
+def send_notification(message):
+    data = {"token": PUSHOVER_TOKEN, "user": PUSHOVER_USER_KEY, "message": message}
+    requests.post("https://api.pushover.net/1/messages.json", data=data)
+
+
+def monitor_website():
+    inital_content = get_content(url)
+    send_notification("The webpage content has changed!")
+
+    while True:
+        current_content = get_content(url)
+
+        if current_content and current_content != inital_content:
+            send_notification("The webpage content has changed!")
+            return
+
+        print(f"{datetime.now()} - No change detected.")
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    monitor_website()
